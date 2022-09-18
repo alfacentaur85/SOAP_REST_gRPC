b517daf2c7ee0b28907a4ff52bc98572c4a1856d
Artur Stepanyants <alfacentaur@yandex.ru>
2022-09-18 13:29:56 +0300

    fix commits

---
+++ Lesson1/LibraryService/LibraryService/Models/Author.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryService.Models
+{
+    /// <summary>
+    /// Модель Автор
+    /// </summary>
+    public class Author
+    {
+        public string Name { get; set; }
+        public string Lang { get; set; }
+
+        public override string ToString() => $"{Name} ({Lang})";
+    }
+}
