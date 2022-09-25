2f42a1a3b8bfc2699b3846436d16122e465cca92
Artur Stepanyants <alfacentaur@yandex.ru>
2022-09-25 21:06:58 +0300

    lesson6

---
+++ Lesson6/RootService/SampleService/OpenAPIs/Impl/RootServiceClient.cs
@@ -0,0 +1,26 @@
+﻿using RootServiceReference;
+using SampleService.Services;
+
+namespace SampleService.OpenAPIs.Impl
+{
+    public class RootServiceClient : IRootServiceClient
+    {
+
+        private readonly ILogger<RootServiceClient> _logger;
+        private RootServiceReference.RootServiceClient _httpClient;
+
+        public RootServiceClient(HttpClient httpClient,
+            ILogger<RootServiceClient> logger)
+        {
+            _logger = logger;
+            _httpClient = new RootServiceReference.RootServiceClient("http://localhost:5075/", httpClient);
+        }
+
+        public RootServiceReference.RootServiceClient Client => _httpClient;
+
+        public async Task<ICollection<WeatherForecast>> Get()
+        {
+            return await _httpClient.GetWeatherForecastAsync();
+        }
+    }
+}
